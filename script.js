665ed295511fa9ed6803212410ad9b301ea1a00d
mostafa mansour <mostafayasser6102001@gmail.com>
2021-11-26 16:25:07 +0200

    make it responsive commit

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-let button = document.getElementById("btn");
-let hero = document.getElementById("hero");
+const button = document.getElementById("btn");
+const hero = document.getElementById("hero");
 let heroStyle = getComputedStyle(hero);
 
 button.onclick = () => {
   if (heroStyle.backgroundColor === "rgb(239, 244, 253)") {
+    // dark mode
     button.style.backgroundColor = "#eed";
     button.style.color = "black";
     button.innerHTML = "Light";
-    hero.style.backgroundColor = "rgb(33, 33, 33)";
+    document.documentElement.style.setProperty("--mode", "rgb(33, 33, 33)");
     document.documentElement.style.setProperty("--main-color", "#eed");
     document.documentElement.style.setProperty("--btn-color", "#212121");
-    console.log("light mode");
   } else {
+    // light mode
     button.style.backgroundColor = "#212121";
     button.style.color = "white";
     button.innerHTML = "Dark";
-    hero.style.backgroundColor = "rgb(239, 244, 253)";
+    document.documentElement.style.setProperty("--mode", "rgb(239, 244, 253)");
     document.documentElement.style.setProperty("--main-color", "#212121");
     document.documentElement.style.setProperty("--btn-color", "#eed");
-    console.log("Dark mode");
   }
+};
+
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
+
+burger.onclick = () => {
+  // toggle the navigation menu
+  navLinks.classList.toggle("nav-active");
+
+  // toggle the navigation button
+  burger.classList.toggle("burger-toggle");
 };
